e10fed1039c72926effe238708a23d854be29b11
Kevin Xue <ivexnuke@gmail.com>
2018-11-16 18:19:22 -0500

    added string vibrating in response to touch

--- Assets/Scripts/GuitarString.cs
+++ Assets/Scripts/GuitarString.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-public class String : MonoBehaviour {
+public class GuitarString : MonoBehaviour {
 	[Range(0, 1)]
 	public float dampRate;
 	public float waveVelocity;
@@ -9,11 +9,12 @@ public class String : MonoBehaviour {
 	public float length;
 	public LineRenderer fretPrefab;
 	public int[] pointsPerFret;
-	float time = 0;
 	LineRenderer lineRenderer;
 	Vector3[] startingPoints;
-	LineSegment[] frets;
+	Vector3[] fretPositions;
+	List<Vibration> vibrations;
 	void Start () {
+		vibrations = new List<Vibration>();
 		lineRenderer = GetComponent<LineRenderer>();
 		InitializeLine();
 		ResizeBox();
@@ -29,7 +30,10 @@ public class String : MonoBehaviour {
 		}
 
 		startingPoints = new Vector3[totalPoints];
-		frets = new LineSegment[pointsPerFret.Length];
+		int[] frets = new int[pointsPerFret.Length + 1];
+		frets[0] = 0;
+		fretPositions = new Vector3[frets.Length];
+
 		float incrementalLength = length / (totalPoints - 1);
 		Vector3 startPosition = transform.position - (Vector3.right * length * 0.5f);
 		Vector3 endPosition = transform.position + (Vector3.right * length * 0.5f);
@@ -40,23 +44,18 @@ public class String : MonoBehaviour {
 		for(int i = 0; i < totalPoints; i++){
 			startingPoints[i] = startPosition + (Vector3.right * incrementalLength * i);
 			if(i - fretStartIndex == pointsPerFret[fretIndex] + 1){
-				LineSegment fret = new LineSegment();
-				fret.startIndex = fretStartIndex;
-				fret.endIndex = i;
 				fretStartIndex = i;
-				frets[fretIndex] = fret;
+				frets[fretIndex + 1] = i;
 				fretIndex++;
 			}
 		}
 
-		LineRenderer newFret = Instantiate(fretPrefab, startingPoints[frets[0].startIndex], Quaternion.identity, transform);
-		newFret.SetPosition(0, Vector3.forward * maxAmplitude);
-		newFret.SetPosition(1, Vector3.forward * -maxAmplitude);
-		foreach (LineSegment fret in frets)
+		for(int i = 0; i < frets.Length; i++)
 		{
-			newFret = Instantiate(fretPrefab, startingPoints[fret.endIndex], Quaternion.identity, transform);
+			LineRenderer newFret = Instantiate(fretPrefab, startingPoints[frets[i]], Quaternion.identity, transform);
 			newFret.SetPosition(0, Vector3.forward * maxAmplitude);
 			newFret.SetPosition(1, Vector3.forward * -maxAmplitude);
+			fretPositions[i] = startingPoints[frets[i]];
 		}
 
 		lineRenderer.positionCount = startingPoints.Length;
@@ -71,9 +70,15 @@ public class String : MonoBehaviour {
 	void Update () {
 		List<StringTouch> touchList = GetRelevantTouches();
 		Vector3[] holds = GetHoldPoints(touchList);
-		// time += Time.deltaTime;
-		// VibrateString(0, startingPoints.Length - 1, startingPoints);
+		Vector3[] releases = GetReleasePoints(touchList);
 		Vector3[] newString = GetStationaryString(holds);
+		UpdateVibrations(releases, holds);
+		foreach (Vibration vibration in vibrations)
+		{
+			int startIndex = FindPointByX(0, newString.Length - 1, newString, fretPositions[vibration.startFret].x);
+			int endIndex = FindPointByX(0, newString.Length - 1, newString, fretPositions[vibration.endFret].x);
+			VibrateString(startIndex, endIndex, newString, vibration.time, vibration.amplitude);
+		}
 		lineRenderer.positionCount = newString.Length;
 		lineRenderer.SetPositions(newString);
 	}
@@ -154,6 +159,19 @@ public class String : MonoBehaviour {
 		touchPoints.Sort((Vector3 a, Vector3 b)=>{return a.x.CompareTo(b.x);});
 		return touchPoints.ToArray();
 	}
+	Vector3[] GetReleasePoints(List<StringTouch> touchList){
+		List<Vector3> touchPoints = new List<Vector3>();
+		foreach (StringTouch touch in touchList)
+		{
+			if(touch.type == TouchType.EXIT){
+				Vector3 localPoint = transform.InverseTransformPoint(touch.hitInfo.point);
+				localPoint.y = 0;
+				touchPoints.Add(localPoint);
+			}
+		}
+		touchPoints.Sort((Vector3 a, Vector3 b)=>{return a.x.CompareTo(b.x);});
+		return touchPoints.ToArray();
+	}
 	Vector3[] GetStationaryString(Vector3[] holdPoints){
 		List<Vector3> newString = new List<Vector3>(startingPoints);
 		newString.AddRange(holdPoints);
@@ -180,7 +198,60 @@ public class String : MonoBehaviour {
 			segment[i] = newPosition;
 		}
 	}
-	void VibrateString(int startIndex, int endIndex, Vector3[] points){
+	void UpdateVibrations(Vector3[] newReleases, Vector3[] holdPoints){
+		foreach (Vector3 release in newReleases)
+		{
+			int startIndex = -1;
+			int endIndex = -1;
+			int[] foundHolds = FindPointsBetweenX(fretPositions[0].x, release.x, holdPoints);
+			
+			if(foundHolds.Length == 0){
+				startIndex = 0;
+			}
+			else{
+				int[] foundFrets = FindPointsBetweenX(holdPoints[foundHolds[foundHolds.Length - 1]].x, release.x, fretPositions);
+				if(foundFrets.Length != 0){
+					startIndex = foundFrets[0];
+				}
+			}
+
+			foundHolds = FindPointsBetweenX(release.x, fretPositions[fretPositions.Length - 1].x, holdPoints);
+			if(foundHolds.Length == 0){
+				endIndex = fretPositions.Length - 1;
+			}
+			else{
+				int[] foundFrets = FindPointsBetweenX(release.x, holdPoints[foundHolds[0]].x, fretPositions);
+				if(foundFrets.Length != 0){
+					endIndex = foundFrets[foundFrets.Length - 1];
+				}
+			}
+			if(startIndex != -1 && endIndex != -1 && startIndex != endIndex){
+				Vibration newVibration = new Vibration();
+				newVibration.startFret = startIndex;
+				newVibration.endFret = endIndex;
+				newVibration.amplitude = maxAmplitude;
+				vibrations.Add(newVibration);
+			}
+		}
+		List<Vibration> removeList = new List<Vibration>();
+		foreach (Vibration vibration in vibrations){
+			if(FindPointsBetweenX(fretPositions[vibration.startFret].x, fretPositions[vibration.endFret].x, holdPoints).Length > 0){
+				removeList.Add(vibration);
+			}
+			else{
+				vibration.time += Time.deltaTime;
+				vibration.amplitude *= dampRate;
+				if(vibration.amplitude < 0.01f){
+					removeList.Add(vibration);
+				}
+			}
+		}
+		foreach (Vibration item in removeList)
+		{
+			vibrations.Remove(item);
+		}
+	}
+	void VibrateString(int startIndex, int endIndex, Vector3[] points, float time, float amplitude){
 		Vector3 firstNode = points[startIndex];
 		Vector3 secondNode = points[endIndex];
 		
@@ -195,7 +266,7 @@ public class String : MonoBehaviour {
 
 		for(int i = 0; i <= totalPoints; i++)
 		{
-			float displacement = maxAmplitude * Mathf.Sin(i * inverseTotal * Mathf.PI) * Mathf.Sin(time * frequency * 4 * Mathf.PI);
+			float displacement = amplitude * Mathf.Sin(i * inverseTotal * Mathf.PI) * Mathf.Sin(time * frequency * 4 * Mathf.PI);
 			points[startIndex + i] = firstNode + (lineDirection * increment * i) + (upDirection * displacement);
 		}
 
@@ -205,14 +276,36 @@ public class String : MonoBehaviour {
         Gizmos.color = new Color(0, 1, 0, 0.75F);
         Gizmos.DrawLine(transform.position - transform.right * length * 0.5f, transform.position + transform.right * length * 0.5f);
     }
+	int FindPointByX(int startIndex, int endIndex, Vector3[] segment, float x){
+		if(startIndex == endIndex && segment[startIndex].x != x){
+			return -1;
+		}
+		int midPoint = (startIndex + endIndex) / 2;
+		if(segment[midPoint].x == x){
+			return midPoint;
+		}
+		else if(x < segment[midPoint].x){
+			return FindPointByX(startIndex, midPoint, segment, x);
+		}
+		else{
+			return FindPointByX(midPoint + 1, endIndex, segment, x);
+		}
+	}
+	int[] FindPointsBetweenX(float minX, float maxX, Vector3[] segment){
+		List<int> points = new List<int>();
+		for(int i = 0; i < segment.Length; i++){
+			if(segment[i].x >= minX && segment[i].x <= maxX){
+				points.Add(i);
+			}
+		}
+		return points.ToArray();
+	}
 }
 public enum TouchType{ENTER, MOVED, EXIT, NONE}
-struct LineSegment{
-	public int startIndex;
-	public int endIndex;
-}
-struct Vibration{
-	public LineSegment segment;
+class Vibration{
+	public int startFret;
+	public int endFret;
+	public float time = 0;
 	public float amplitude;
 }
 
